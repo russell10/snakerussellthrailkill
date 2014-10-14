25f1b41681b5b018986c23b58336b05fd9d63779
russellthrailkill <thrailkill39@Gmail>
2014-10-14 14:04:19 -0700

    snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,5 +1,48 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
-var screenHeight;
+var screenHeight; 
+
+gameInitialize();
+gameDraw(); 
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d"); 
+
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(255, 0, 47)";
+    context.fillRect(0,0, screenWidth, screenHeight);
+}
+
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++) {
+        
+    }
+}
+
+function snakeDraw() {
+    
+} 
+
+function snakeUpdate() {
+    
+}
